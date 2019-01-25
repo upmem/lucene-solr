0158bb8b71984fd206aba8e0bf495a32c26ac0a8
Romaric JODIN <rjodin@upmem.com>
2019-01-25 18:59:24 +0100

    x86_poc: main: add parsing of .si/.cfe in main for starter

--- lucene-dpu/c/x86_poc/main.c
+++ lucene-dpu/c/x86_poc/main.c
@@ -1,3 +1,11 @@
-int main() {
+#include "parser.h"
+
+int main(int argc, char **argv)
+{
+    void **file_buffers;
+
+    file_buffers = get_file_buffers(argv[1], 0);
+    free_file_buffers(file_buffers);
+
     return 0;
 }
